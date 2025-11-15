e50effbdc5b2562b323ef2bc281fc5b05e2f32b7
Liam Lethbridge <69471164+MrClever101@users.noreply.github.com>
2025-11-15 17:48:47 +0000

    coding test solutions

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -1,7 +1,8 @@
 <script lang="ts">
   import Todo from "./lib/Todo.svelte";
   import type { TodoItem } from "./lib/types";
-
+  let title = $state("");
+  let description = $state("");
   let todos: TodoItem[] = $state([]);
 
   async function fetchTodos() {
@@ -18,10 +19,48 @@
     }
   }
 
+  async function addTodo(event: SubmitEvent) {
+    // in my opinon, submitting the form normally and causing a reload is janky and annoying. I think preventing the default is more seemless
+    event.preventDefault()
+
+    // make sure the inputs are valid
+    if(title === "" || description === "" ){
+      // ideally I would add something like a pop-up to let the user know but I didn't want to mess with the layout of the webpage
+      console.log("Invalid inputs")
+      return
+    }
+
+    try{
+      const response = await fetch("http://localhost:8080/",{
+      method: "POST",
+      headers: { "Content-Type": "application/json"},
+      body: JSON.stringify({title, description})
+    });
+      // If we get the item back, we know it has been processes correctly and can push to client list without needing to get the entire list back
+      const todo = await response.json() as TodoItem
+      // check if todo is null. Very annoying svelte reactive property
+      if(todos){
+        todos.push(todo)
+      }else{
+        todos = [todo]
+      }  
+      title = ""
+      description = ""
+  } catch (e) {
+      console.error("Error processing Todo.")
+  }
+}
+
   // Initially fetch todos on page load
   $effect(() => {
     fetchTodos();
   });
+
+/*
+I was originally going to add functionality like "crossing a task off" 
+but to have this task permanently crossed off I would need extra functionality on the backend   
+and the OpenAPI spec does not include any functionality for marking a task as completed or removing it from the list so I left it
+*/
 </script>
 
 <main class="app">
@@ -36,9 +75,9 @@
   </div>
 
   <h2 class="todo-list-form-header">Add a Todo</h2>
-  <form class="todo-list-form">
-    <input placeholder="Title" name="title" />
-    <input placeholder="Description" name="description" />
+  <form class="todo-list-form" onsubmit={addTodo}>
+    <input placeholder="Title" name="title" bind:value={title} minlength="1"/>
+    <input placeholder="Description" name="description" id="description" bind:value={description} minlength="1"/>
     <button>Add Todo</button>
   </form>
 </main>
